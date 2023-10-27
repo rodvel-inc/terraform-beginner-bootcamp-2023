3f8ea1e339e638ade658cea1352f034fff10d272
rodvel-inc <ajvelandiae@gmail.com>
2023-10-27 12:58:11 +0000

    #31 Invalidate CDN cache on content_version changes.

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -63,6 +63,19 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+ 
+}
 
-  
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+
+  provisioner "local-exec" {
+    command = "aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.s3_distribution.id} --paths '/*'"
+    
+  #  command = <<COMMAND 
+#aws cloudfront create-invalidation \
+#--distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+#--paths "/*"
+#    COMMAND
+  }
 }
