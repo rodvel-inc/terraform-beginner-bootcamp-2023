6e5b8bd7bfe3a8af5b7cf043f1794dca76b2534c
rodvel-inc <ajvelandiae@gmail.com>
2023-11-07 18:54:23 +0000

    #47 implement tf provier block in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "s3_bucket_name" {
-  description = "The name of the bucket for the website"
-  value = module.terrahouse_aws.bucket_name
-}
+#output "s3_bucket_name" {
+#  description = "The name of the bucket for the website"
+#  value = module.terrahouse_aws.bucket_name
+#}
 
-output "s3_website_endpoint" {
-  description = "s3 static Website endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+#output "s3_website_endpoint" {
+#  description = "s3 static Website endpoint"
+#  value = module.terrahouse_aws.website_endpoint
+#}
 
-output "cloudfront_url" {
-  value = module.terrahouse_aws.cloudfront_url
-}
+#output "cloudfront_url" {
+#  value = module.terrahouse_aws.cloudfront_url
+#}
