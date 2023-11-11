f60f02bcbeb63b28cddd8caebc9a927887700842
rodvel-inc <110110374+rodvel-inc@users.noreply.github.com>
2023-11-11 07:40:10 -0500

    #50 Test terrahome against the real terratowns (#44)

--- main.tf
+++ main.tf
@@ -8,26 +8,35 @@ terraform {
     }
 }
 
-provider "terratowns" {
-    endpoint = "http://localhost:4567/api"
-    user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-    token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
-}
-
-#module "terrahouse_aws" {
-#    source = "./modules/terrahouse_aws"
-#    user_uuid = var.user_uuid
-#    bucket_name = var.bucket_name
-#    index_html_filepath = var.index_html_filepath
-#    error_html_filepath = var.error_html_filepath
-#    content_version = var.content_version
+# this endpoint is the local terratowns, a local emulator
+#provider "terratowns" {
+#    endpoint = "http://localhost:4567/api"
+#    user_uuid=var.teacherseat_user_uuid 
+#    token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 #}
 
+#this is the real Terratowns
+provider "terratowns" {
+    endpoint = var.terratowns_endpoint
+    user_uuid = var.teacherseat_user_uuid
+    token=var.terratowns_access_token
+}
+
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = var.teacherseat_user_uuid
+    bucket_name = var.bucket_name
+    index_html_filepath = var.index_html_filepath
+    error_html_filepath = var.error_html_filepath
+    content_version = var.content_version
+}
+
 resource "terratowns_home" "home" {
-  name = "metal rodvel" 
-  description = "best death metal bands ever"
-  town = "gamers-grotto"
+  name = "Metal Rodvel" 
+  description = "Best Death Metal bands ever!"
+  town = "missingo"
   content_version = 1
-  domain_name = "dfrga1978.cloudfront.net"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3faasd13c.cloudfront.net"
   
 }
